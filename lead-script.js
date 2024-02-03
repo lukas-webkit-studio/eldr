a021bdc8af8540960cd4ccef7b20a98a5ece76c1
Lukáš Svoboda <158773258+lukas-webkit-studio@users.noreply.github.com>
2024-02-04 00:29:56 +0100

    Adding lead script

---
+++ lead-script.js
@@ -0,0 +1,7 @@
+!(function (l, s) {
+l.type = "text/javascript";
+l.async = true;
+l.src = "https://" + "ct.l" + "eady.com/R6vz1S8NAqk4NrQ8/L" + ".js";
+s = s[0];
+s.parentNode.insertBefore(l, s);
+})(document.createElement("script"), document.getElementsByTagName("script"));
