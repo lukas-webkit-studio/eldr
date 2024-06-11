0f97931f72f8d666f9fea62118434defc3e95db8
Lukáš Svoboda <158773258+lukas-webkit-studio@users.noreply.github.com>
2024-06-11 22:22:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Adding class based on the lang of the current Localization
 document.addEventListener('DOMContentLoaded', function() {
   const url = window.location.href;
 
@@ -9,4 +8,41 @@ document.addEventListener('DOMContentLoaded', function() {
   } else {
     document.body.classList.add('lang-cs'); // Default to Czech if no language is specified in the URL
   }
+
+  // Function to update the language text based on the body class
+  function updateLanguageText() {
+    const languageElements = document.querySelectorAll('.SectionReference__Collection__Item__Language');
+    languageElements.forEach(function(element) {
+      const languageText = element.textContent.trim().toLowerCase();
+      const bodyClass = document.body.classList;
+
+      if (bodyClass.contains('lang-cs')) {
+        if (languageText === 'cs') {
+          element.style.display = 'none';
+        } else if (languageText === 'de') {
+          element.textContent = 'Přeloženo z němčiny';
+        } else if (languageText === 'en') {
+          element.textContent = 'Přeloženo z angličtiny';
+        }
+      } else if (bodyClass.contains('lang-de')) {
+        if (languageText === 'de') {
+          element.style.display = 'none';
+        } else if (languageText === 'cs') {
+          element.textContent = 'Aus dem Tschechischen übersetzt';
+        } else if (languageText === 'en') {
+          element.textContent = 'Aus dem Englischen übersetzt';
+        }
+      } else if (bodyClass.contains('lang-en')) {
+        if (languageText === 'en') {
+          element.style.display = 'none';
+        } else if (languageText === 'cs') {
+          element.textContent = 'Translated from Czech';
+        } else if (languageText === 'de') {
+          element.textContent = 'Translated from German';
+        }
+      }
+    });
+  }
+
+  updateLanguageText();
 });
