c2e954e4cd00f4da687c0a48a44c9907096aa2b2
Lukáš Svoboda <158773258+lukas-webkit-studio@users.noreply.github.com>
2024-12-16 01:56:55 +0100

    Update show-mode.js

--- show-mode.js
+++ show-mode.js
@@ -4,29 +4,27 @@
 const btn = document.querySelectorAll(".longtext__button");
 const text = document.querySelectorAll(".longtext");
 
-// Visibility check
-window.onload = function () {
-  if ($(window).width() < 992) {
-    btn.forEach(function (button, index) {
-      if (text[index].offsetHeight >= 180) {
-        text[index].style.height = "220px";
-        button.style.display = "block";
-      } else {
-        button.style.display = "none";
-      }
-    });
-  }
-};
+// Ensure elements exist before proceeding
+if (btn.length > 0 && text.length > 0) {
+  // Visibility check
+  window.onload = function () {
+    if (window.innerWidth < 992) {
+      btn.forEach((button, index) => {
+        if (text[index].offsetHeight >= 180) {
+          text[index].style.height = "220px";
+          button.style.display = "block";
+        } else {
+          button.style.display = "none";
+        }
+      });
+    }
+  };
 
-// Action on click
-btn.onclick = function (button) {
-  const index = Array.indexOf(button, btn);
-  text[index].style.height = "auto";
-  button.style.display = "none";
-};
-btn.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    button.style.display = "none";
-    text[index].style.height = "auto";
+  // Add click events
+  btn.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      text[index].style.height = "auto";
+      button.style.display = "none";
+    });
   });
-});
+}
